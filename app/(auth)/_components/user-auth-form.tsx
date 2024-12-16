14dc6828566a7c41e3074e57155b31cc8f6c6bb6
naftalimurgor <murgornaftali@gmail.com>
2024-12-16 17:13:35 +0300

    cleanup

--- app/(auth)/_components/user-auth-form.tsx
+++ app/(auth)/_components/user-auth-form.tsx
@@ -16,7 +16,6 @@ import { useTransition } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import * as z from 'zod';
-import GithubSignInButton from './github-auth-button';
 
 const formSchema = z.object({
   email: z.string().email({ message: 'Enter a valid email address' })
@@ -61,33 +60,29 @@ export default function UserAuthForm() {
                 <FormLabel>Email</FormLabel>
                 <FormControl>
                   <Input
-                    type="email"
-                    placeholder="Enter your email..."
+                    type="password"
+                    placeholder="Password"
                     disabled={loading}
                     {...field}
                   />
                 </FormControl>
+                <FormLabel>Re-enter password</FormLabel>
+                <Input
+                  type="password"
+                  placeholder="Enter your password..."
+                  disabled={loading}
+                  {...field}
+                />
                 <FormMessage />
               </FormItem>
             )}
           />
 
           <Button disabled={loading} className="ml-auto w-full" type="submit">
-            Continue With Email
+            Continue
           </Button>
         </form>
       </Form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <GithubSignInButton />
     </>
   );
 }
